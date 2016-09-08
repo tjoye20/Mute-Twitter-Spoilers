03a06d41ff192157eac7539df897314cb9ed3ffe
Tolu Oyeniyi <tjoye20@gmail.com>
2016-09-08 14:51:32 -0500

    Setup routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users
+
+  resources :mutedphrases do
+    resources :blockedfollowers
+  end
+
+  resources :sessions, only: [:destroy]
+
+  get '/sign-in', to: "users#new", as: "sign-in"
+  get '/logout', to: "sessions#destroy", as: "logout"
+
+  root "users#new"
 end
