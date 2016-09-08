a01e7ccc4e5016b766fc6f0047349fdf9365977d
Tolu Oyeniyi <tjoye20@gmail.com>
2016-09-08 16:46:07 -0500

    Add failing user model spec.

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  let!(:valid_user) {User.create(username: "tjo", token: "hfajf8uuafaj", secret: "tlkjer09984r0=3")}
+  let(:invalid_user) {User.create(username: "tj", token: "", secret: "")}
+
+  context "when passed valid params" do
+    it "creates a user" do
+      expect(valid_user.valid?).to eq true
+      expect(valid_user.username).to eq "tjo"
+      expect(User.all).to eq [valid_user]
+    end
+  end
+
+  context "when params are invalid" do
+    it "raises errors" do
+      expect(invalid_user.valid?).to eq false
+      expect(invalid_user.username).to eq nil
+      expect(User.all).to eq [valid_user]
+    end
+  end
+end
