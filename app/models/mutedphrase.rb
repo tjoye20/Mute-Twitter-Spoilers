0921fe909c5a312e70e8c412f4727c46883b1eec
Tolu Oyeniyi <tjoye20@gmail.com>
2016-09-08 18:24:14 -0500

    Add passing mutedphrase spec and update model.

--- app/models/mutedphrase.rb
+++ app/models/mutedphrase.rb
@@ -1,4 +1,7 @@
 class Mutedphrase < ApplicationRecord
   belongs_to :user
   has_many :mutedfollowers
+
+  validates_presence_of :phrase
+  validates_length_of :phrase, minimum: 1, too_short: "This entry is too short."
 end
--- spec/models/mutedphrase_spec.rb
+++ spec/models/mutedphrase_spec.rb
@@ -5,13 +5,19 @@ RSpec.describe Mutedphrase, type: :model do
 
   context "when valid params are passed" do
     it "saves a new muted phrase" do
-
+      new_phrase = valid_user.mutedphrases.create(phrase: "test phrase")
+      expect(new_phrase.valid?).to eq true
+      expect(Mutedphrase.all).to eq [new_phrase]
+      expect(new_phrase.phrase).to eq "test phrase"
     end
   end
 
   context "when invalid params are passed" do
     it "raises errors and doesn't save a new record" do
-
+      new_phrase = valid_user.mutedphrases.create(phrase: "")
+      expect(new_phrase.valid?).to eq false
+      expect(Mutedphrase.all.length).to eq 0
+      expect(new_phrase.id).to eq nil
     end
   end
 
