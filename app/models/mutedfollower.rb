aeb21b9b2ae56380387e5c9fb090e3fb8d40f999
Tolu Oyeniyi <tjoye20@gmail.com>
2016-09-08 19:02:29 -0500

    Add first part of passing mutedfollower spec.

--- app/models/mutedfollower.rb
+++ app/models/mutedfollower.rb
@@ -1,4 +1,6 @@
 class Mutedfollower < ApplicationRecord
   belongs_to :user
-  belongs_to :mutedphrase 
+  belongs_to :mutedphrase
+
+  validates_presence_of :screen_name
 end
--- spec/models/mutedfollower_spec.rb
+++ spec/models/mutedfollower_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Mutedfollower, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:valid_user) {User.create(username: "tjo", token: "hfajf8uuafaj", secret: "tlkjer09984r0=3")}
+  let!(:new_phrase) {valid_user.mutedphrases.create(phrase: "test phrase")}
+
+  context "when valid params are passed" do
+    it "saves the muted follower" do
+      new_muted_follower = new_phrase.mutedfollowers.create(screen_name: "tjonem", user_id: valid_user.id)
+      expect(new_muted_follower.valid?).to eq true
+      expect(Mutedfollower.all).to eq [new_muted_follower]
+      expect(new_muted_follower.id).to eq 1
+      expect(new_phrase.mutedfollowers).to eq [new_muted_follower]
+    end
+  end
+
+  context "when invalid params are passed" do
+
+  end
 end
