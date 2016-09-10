d87a0380cf6f0e0cbd2d309e91bda8f63c42556b
Tolu Oyeniyi <tjoye20@gmail.com>
2016-09-10 16:09:49 -0500

    Pass all model specs.

--- app/models/mutedfollower.rb
+++ app/models/mutedfollower.rb
@@ -1,4 +1,7 @@
 class Mutedfollower < ApplicationRecord
   belongs_to :user
   belongs_to :mutedphrase
+
+  validates_presence_of :screen_name
+  validates_length_of :screen_name, minimum: 1, too_short: "This entry is too short."
 end
