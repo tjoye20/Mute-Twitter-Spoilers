590297320f085349311bacfc1704aa23359ba4b5
Tolu Oyeniyi <tjoye20@gmail.com>
2016-09-13 20:12:48 -0500

    Add helpers and update routing.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,4 +18,41 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def search_results(phrase_to_block)
+    user_timeline = client.home_timeline(
+    count: 100, exclude_replies: true)
+    @results = []
+    user_timeline.each do |tweet|
+      if tweet.text.include?(phrase_to_block)
+        @results << tweet
+      end
+    end
+    @results
+  end
+
+  def mute_phrase(phrase_to_block)
+    results = search_results(phrase_to_block)
+    new_blocked_phrase = BlockedPhrase.create(
+    phrase: phrase_to_block,
+    user_id: current_user.id)
+
+    results.each do |tweet|
+      client.mute(tweet.user.screen_name)
+      BlockedFollower.create(
+      blocked_phrase_id: new_blocked_phrase.id,
+      follower: tweet.user.screen_name,
+      blocked_tweet: tweet.text)
+    end
+  end
+
+  def unmute_phrase(blocked_phrase_id)
+    phrase = BlockedPhrase.find(blocked_phrase_id)
+    followers = BlockedFollower.where(blocked_phrase_id: phrase.id)
+    followers.each do |user|
+      client.unmute(user.follower)
+    end
+    phrase.destroy
+    followers.destroy_all
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:create]
 
-  resources :mutedphrases, only: [:index, :new, :create, :destroy] do
-    resources :blockedfollowers, only: [:index, :show, :create]
-  end
+  resources :mutedphrases, only: [:index, :new, :create, :show, :show_tweets, :destroy]
 
   resources :sessions, only: [:new, :destroy, :failure]
 
