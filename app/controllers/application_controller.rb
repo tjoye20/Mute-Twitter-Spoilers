8a5b62adca3626ca0a28f12ea758f338c52bedf4
Tolu Oyeniyi <tjoye20@gmail.com>
2016-09-14 13:18:02 -0500

    Add navbar partial. Update application controller helper method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  helper_method :current_user
   helper_method :all
-
+  
   private
 
   def current_user
-    @current_user ||= (User.find(session[:user_id]) if session[:user_id])
+    @current_user ||= User.find(session[:user_id])
   end
 
   def client
